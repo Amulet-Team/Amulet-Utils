c9208df5bec89839fe549fe66f003ae51de592f3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-30 09:27:44 +0100

    Add logging, signal and weak implementation

---
+++ src/amulet/utils/__init__.py
@@ -0,0 +1,25 @@
+from . import _version
+__version__ = _version.get_versions()['version']
+
+def _init() -> None:
+    import os
+    import sys
+    import ctypes
+
+    if sys.platform == "win32":
+        lib_path = os.path.join(os.path.dirname(__file__), "amulet_utils.dll")
+    elif sys.platform == "darwin":
+        lib_path = os.path.join(os.path.dirname(__file__), "libamulet_utils.dylib")
+    elif sys.platform == "linux":
+        lib_path = os.path.join(os.path.dirname(__file__), "libamulet_utils.so")
+    else:
+        raise RuntimeError(f"Unsupported platform {sys.platform}")
+
+    # Load the shared library
+    ctypes.cdll.LoadLibrary(lib_path)
+
+    from ._utils import init
+
+    init(sys.modules[__name__])
+
+_init()
