64f483959a9fdf3af67894e92753df00ee382c1f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-01 14:54:53 +0100

    Remove scope

--- src/amulet/utils/signal/signal.cpp
+++ src/amulet/utils/signal/signal.cpp
@@ -46,27 +46,25 @@ namespace detail {
 
     void EventLoop::_event_loop()
     {
-        {
-            std::unique_lock lock(_mutex);
-            while (!_exit) {
-                if (_events.empty()) {
-                    // If there are no events to process, wait until more are added.
-                    _condition.wait(lock);
-                    // Re-check the exit condition.
-                    continue;
-                }
-                auto event = _events.front();
-                _events.pop_front();
-                lock.unlock();
-                try {
-                    event();
-                } catch (const std::exception& e) {
-                    Amulet::error(std::string("Unhandled exception in event loop: ") + e.what());
-                } catch (...) {
-                    Amulet::error("Unhandled exception in event loop.");
-                }
-                lock.lock();
+        std::unique_lock lock(_mutex);
+        while (!_exit) {
+            if (_events.empty()) {
+                // If there are no events to process, wait until more are added.
+                _condition.wait(lock);
+                // Re-check the exit condition.
+                continue;
             }
+            auto event = _events.front();
+            _events.pop_front();
+            lock.unlock();
+            try {
+                event();
+            } catch (const std::exception& e) {
+                Amulet::error(std::string("Unhandled exception in event loop: ") + e.what());
+            } catch (...) {
+                Amulet::error("Unhandled exception in event loop.");
+            }
+            lock.lock();
         }
         if (get_min_log_level() <= 10) {
             std::cout << "Exiting event loop" << std::endl;
