09bd5dece056692a40f653cf02277581f86ac25f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-02 09:52:04 +0100

    Add debug messages

--- src/amulet/utils/logging/logging.cpp
+++ src/amulet/utils/logging/logging.cpp
@@ -42,26 +42,31 @@ void debug(const std::string& msg)
 
 void info(const std::string& msg)
 {
+    std::cout << "info" << std::endl;
     log(20, msg);
 }
 
 void warning(const std::string& msg)
 {
+    std::cout << "warning" << std::endl;
     log(30, msg);
 }
 
 void error(const std::string& msg)
 {
+    std::cout << "error" << std::endl;
     log(40, msg);
 }
 
 void critical(const std::string& msg)
 {
+    std::cout << "critical" << std::endl;
     log(50, msg);
 }
 
 static void default_log_handler(int level, const std::string& msg)
 {
+    std::cout << "log default" << std::endl;
     static std::mutex log_mutex;
     std::unique_lock lock(log_mutex);
     std::cout << msg << std::endl;
