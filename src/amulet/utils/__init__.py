4240d14e87de6676683f50891b5d41371e2dace1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-05 09:26:37 +0100

    Implement file locking, array unique and temporary directories (#8)
    * Add lock file, unique
    
    * Fix and improve unique
    
    * Fix lock file tests
    
    * Export TempDir
    
    * Add TempDir tests
    
    * Reformat
    
    * Set default CACHE_DIR
    
    * Ensure cache directory exists

--- src/amulet/utils/__init__.py
+++ src/amulet/utils/__init__.py
@@ -7,6 +7,12 @@ def _init() -> None:
     import os
     import sys
     import ctypes
+    import platformdirs
+
+    os.environ.setdefault(
+        "CACHE_DIR", platformdirs.user_cache_dir("AmuletTeam", "AmuletTeam")
+    )
+    os.makedirs(os.environ["CACHE_DIR"], exist_ok=True)
 
     if sys.platform == "win32":
         lib_path = os.path.join(os.path.dirname(__file__), "amulet_utils.dll")
