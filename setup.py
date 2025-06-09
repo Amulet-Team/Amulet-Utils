9d7806afece0c7bdf985d4649475420f3e8242f1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-09 10:31:00 +0100

    Move requirements into a new file

--- setup.py
+++ setup.py
@@ -8,48 +8,15 @@ from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
 
 from packaging.version import Version
+
 import versioneer
 
+import requirements
 
 def fix_path(path: str) -> str:
     return os.path.realpath(path).replace(os.sep, "/")
 
 
-dependencies = [
-    "amulet-compiler-target==1.0",
-    "platformdirs~=3.1",
-]
-
-def add_cpp_dependency(lib_name: str, version_str: str) -> None:
-    version = Version(version_str)
-    if version.is_prerelease:
-        # Breaking API changes can be made between pre-release versions. Pin to this exact release.
-        dependencies.append(f"{lib_name}=={version_str}")
-    else:
-        # Major - breaking API change. Dependents must be updated and recompiled.
-        major = version.major
-        # Minor - backwards compatible API change. Dependents must be recompiled.
-        minor = version.minor
-        # Patch - API unchanged. Dependents must be recompiled.
-        patch = version.micro
-        # Fix - API unchanged. Dependents do not need to be recompiled.
-        dependencies.append(f"{lib_name}~={major}.{minor}.{patch}.0")
-
-try:
-    import amulet_compiler_version
-except ImportError:
-    dependencies.append("amulet-compiler-version==1.3.0")
-else:
-    dependencies.append(
-        f"amulet-compiler-version=={amulet_compiler_version.__version__}"
-    )
-
-try:
-    import amulet.pybind11_extensions
-except ImportError:
-    dependencies.append("amulet_pybind11_extensions~=1.0")
-else:
-    add_cpp_dependency("amulet_pybind11_extensions", amulet.pybind11_extensions.__version__)
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
 
@@ -136,5 +103,5 @@ setup(
     version=_get_version(),
     cmdclass=cmdclass,
     ext_modules=[Extension("amulet.utils._amulet_utils", [])],
-    install_requires=dependencies,
+    install_requires=requirements.get_runtime_dependencies(),
 )
