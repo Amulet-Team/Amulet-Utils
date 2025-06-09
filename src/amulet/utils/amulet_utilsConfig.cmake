8495a5f8b8c5c5a9aa13662dfbb38f68d4c20b79
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-09 16:42:05 +0100

    Fix cmake config (#23)

--- src/amulet/utils/amulet_utilsConfig.cmake
+++ src/amulet/utils/amulet_utilsConfig.cmake
@@ -5,9 +5,12 @@ if (NOT TARGET amulet_utils)
     find_library(amulet_utils_LIBRARY NAMES amulet_utils PATHS "${CMAKE_CURRENT_LIST_DIR}")
     message(STATUS "amulet_utils_LIBRARY: ${amulet_utils_LIBRARY}")
 
-    add_library(amulet_utils SHARED IMPORTED)
-    set_target_properties(amulet_utils PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${amulet_utils_INCLUDE_DIR}"
+    add_library(amulet_utils_bin SHARED IMPORTED)
+    set_target_properties(amulet_utils_bin PROPERTIES
         IMPORTED_IMPLIB "${amulet_utils_LIBRARY}"
     )
+
+    add_library(amulet_utils INTERFACE)
+    target_link_libraries(amulet_utils INTERFACE amulet_utils_bin)
+    target_include_directories(amulet_utils INTERFACE ${amulet_utils_INCLUDE_DIR})
 endif()
