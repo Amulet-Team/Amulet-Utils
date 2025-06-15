50af22d9277d22aa9778948d02a5fc709e8bfd4c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-15 15:18:30 +0100

    Add pybind11 dependency

--- src/amulet/utils/amulet_utilsConfig.cmake
+++ src/amulet/utils/amulet_utilsConfig.cmake
@@ -1,6 +1,8 @@
 if (NOT TARGET amulet_utils)
     message(STATUS "Finding amulet_utils")
 
+    find_package(pybind11 CONFIG REQUIRED)
+
     set(amulet_utils_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
     find_library(amulet_utils_LIBRARY NAMES amulet_utils PATHS "${CMAKE_CURRENT_LIST_DIR}")
     message(STATUS "amulet_utils_LIBRARY: ${amulet_utils_LIBRARY}")
@@ -11,6 +13,7 @@ if (NOT TARGET amulet_utils)
     )
 
     add_library(amulet_utils INTERFACE)
+    target_link_libraries(amulet_utils INTERFACE pybind11::module)
     target_link_libraries(amulet_utils INTERFACE amulet_utils_bin)
     target_include_directories(amulet_utils INTERFACE ${amulet_utils_INCLUDE_DIR})
 endif()
