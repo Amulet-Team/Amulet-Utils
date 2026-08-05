942e3e1b4c12f7ef09bc289c0278670c1d7737cc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-05 15:03:27 +0100

    Modify lock_guard and unique_lock to inherit std
    Instead of having a custom implementation, this inherits the std classes and adds annotations

--- src/amulet/utils/threading/mutex.hpp
+++ src/amulet/utils/threading/mutex.hpp
@@ -19,51 +19,44 @@ public:
 };
 
 template<class Mutex>
-class [[nodiscard]] ASTD_SCOPED_CAPABILITY lock_guard {
-private:
-    Mutex& _m;
-
+class [[nodiscard]] ASTD_SCOPED_CAPABILITY lock_guard : private std::lock_guard<Mutex> {
 public:
-    using mutex_type = Mutex;
+    using std::lock_guard<Mutex>::lock_guard;
 
-    explicit lock_guard( Mutex& m ) ASTD_ACQUIRE(m) : _m(m) { m.lock(); }
-    lock_guard( Mutex& m, std::adopt_lock_t ) ASTD_REQUIRES(m) : _m(m) {}
+    explicit lock_guard( Mutex& m ) ASTD_ACQUIRE(m) : std::lock_guard<Mutex>(m) {}
+    lock_guard( Mutex& m, std::adopt_lock_t ) ASTD_REQUIRES(m) : std::lock_guard<Mutex>(m, std::adopt_lock) {}
     lock_guard( const lock_guard& ) = delete;
     lock_guard& operator=( const lock_guard& ) = delete;
-    ~lock_guard() ASTD_RELEASE() { _m.unlock(); }
+    ~lock_guard() ASTD_RELEASE() {}
 };
 
 template<class Mutex>
-class [[nodiscard]] ASTD_SCOPED_CAPABILITY unique_lock {
-private:
-    Mutex* _m;
-    bool owns;
-
+class [[nodiscard]] ASTD_SCOPED_CAPABILITY unique_lock : private std::unique_lock<Mutex> {
 public:
     using mutex_type = Mutex;
 
     // Constructors
-    unique_lock() noexcept : _m(nullptr), owns(false) {}
+    unique_lock() noexcept : std::unique_lock<Mutex>() {}
 
     explicit unique_lock( Mutex& m ) ASTD_ACQUIRE(m)
-        : _m(&m), owns(true) { m.lock(); }
+        : std::unique_lock<Mutex>(m) {}
 
     unique_lock( Mutex& m, std::defer_lock_t ) noexcept ASTD_EXCLUDES(m)
-        : _m(&m), owns(false) {}
+        : std::unique_lock<Mutex>(m, std::defer_lock) {}
 
     unique_lock( Mutex& m, std::adopt_lock_t ) ASTD_REQUIRES(m)
-        : _m(&m), owns(true) {}
+        : std::unique_lock<Mutex>(m, std::adopt_lock) {}
 
     unique_lock( Mutex& m, std::try_to_lock_t ) ASTD_EXCLUDES(m)
-        : _m(&m), owns(m.try_lock()) {}
+        : std::unique_lock<Mutex>(m, std::try_to_lock) {}
 
     template< class Rep, class Period >
     unique_lock( Mutex& m, const std::chrono::duration<Rep, Period>& timeout_duration ) ASTD_EXCLUDES(m)
-        : _m(&m), owns(m.try_lock_for(timeout_duration)) {}
+        : std::unique_lock<Mutex>(m, timeout_duration) {}
 
     template< class Clock, class Duration >
     unique_lock( Mutex& m, const std::chrono::time_point<Clock, Duration>& timeout_time ) ASTD_EXCLUDES(m)
-        : _m(&m), owns(m.try_lock_until(timeout_time)) {}
+        : std::unique_lock<Mutex>(m, timeout_time) {}
 
     // Copy
     unique_lock( unique_lock& other ) = delete;
@@ -72,69 +65,65 @@ public:
     // Move
     // TODO: Add annotations if Clang supports moving, swapping and disowning capabilities
     unique_lock( unique_lock&& other ) noexcept
-        : _m(std::exchange(other._m, nullptr))
-        , owns(std::exchange(other.owns, false)) {}
+        : std::unique_lock<Mutex>(std::forward<unique_lock>(other)) {}
 
     unique_lock& operator=( unique_lock&& other ) noexcept
     {
-        if (this != &other) {
-            if (_m && owns) _m->unlock();
-            _m = std::exchange(other._m, nullptr);
-            owns = std::exchange(other.owns, false);
-        }
-        return *this;
+        return std::unique_lock<Mutex>::operator=(std::forward<unique_lock>(other));
     }
 
     void swap( unique_lock& other ) noexcept {
-        std::swap(_m, other._m);
-        std::swap(owns, other.owns);
+        std::unique_lock<Mutex>::swap(other);
     }
 
-    mutex_type* release() noexcept ASTD_RETURN_CAPABILITY(m)  {
-        owns = false;
-        return std::exchange(_m, nullptr);
+    mutex_type* release() noexcept {
+        return std::unique_lock<Mutex>::release();
     }
 
     // Destructor
-    ~unique_lock() ASTD_RELEASE() { if (_m && owns) _m->unlock(); }
+    ~unique_lock() ASTD_RELEASE() {}
 
     void lock() ASTD_ACQUIRE() {
-        _m->lock();
-        owns = true;
+        std::unique_lock<Mutex>::lock();
     }
 
     [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE(true) {
-        return owns = _m->try_lock();
+        return std::unique_lock<Mutex>::try_lock();
     }
 
     template< class Rep, class Period >
     [[nodiscard]] bool try_lock_for( const std::chrono::duration<Rep, Period>& timeout_duration ) ASTD_TRY_ACQUIRE(true) {
-        return owns = _m->try_lock_for(timeout_duration);
+        return std::unique_lock<Mutex>::try_lock_for(timeout_duration);
     }
 
     template< class Clock, class Duration >
     [[nodiscard]] bool try_lock_until( const std::chrono::time_point<Clock, Duration>& timeout_time ) ASTD_TRY_ACQUIRE(true) {
-        return owns = _m->try_lock_until(timeout_time);
+        return std::unique_lock<Mutex>::try_lock_until(timeout_time);
     }
 
     void unlock() ASTD_RELEASE() {
-        _m->unlock();
-        owns = false;
+        std::unique_lock<Mutex>::unlock();
     }
 
     // Clang thread safety analysis does not support alias analysis. Do not use this.
-    mutex_type* mutex() const noexcept { return _m; }
+    mutex_type* mutex() const noexcept {
+        return std::unique_lock<Mutex>::mutex();
+    }
 
     // TODO: The following methods require a non-acquiring version of TRY_ACQUIRE
-    bool owns_lock() const noexcept { return owns; }
-    explicit operator bool() const noexcept { return owns; }
+    bool owns_lock() const noexcept {
+        return std::unique_lock<Mutex>::owns_lock();
+    }
+    explicit operator bool() const noexcept {
+        return std::unique_lock<Mutex>::operator bool();
+    }
 };
 
 } // namespace astd
 
 template< class Mutex >
-void swap( unique_lock<Mutex>& lhs,
-           unique_lock<Mutex>& rhs ) noexcept {
+void swap( astd::unique_lock<Mutex>& lhs,
+           astd::unique_lock<Mutex>& rhs ) noexcept {
     lhs.swap(rhs);
 }
 
