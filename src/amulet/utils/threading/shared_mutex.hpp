d05a547942a509c2ebc1600c7c9cbb6fd172f51c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-10 17:23:46 +0100

    Add shared mutex and lock

---
+++ src/amulet/utils/threading/shared_mutex.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <shared_mutex>
+
+#if defined(AMULET_THREAD_SAFETY_ANALYSIS)
+
+#include <chrono>
+
+#include "thread_safety.hpp"
+
+namespace astd {
+
+class ASTD_CAPABILITY("shared mutex") shared_mutex : private std::shared_mutex {
+public:
+    using std::shared_mutex::shared_mutex;
+    using std::shared_mutex::native_handle_type;
+    using std::shared_mutex::native_handle;
+    void lock() ASTD_ACQUIRE() { std::shared_mutex::lock(); }
+    void lock_shared() ASTD_ACQUIRE_SHARED() { std::shared_mutex::lock_shared(); }
+    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE(true) { return std::shared_mutex::try_lock(); }
+    [[nodiscard]] bool try_lock_shared() ASTD_TRY_ACQUIRE_SHARED(true) { return std::shared_mutex::try_lock_shared(); }
+    void unlock() ASTD_RELEASE() { std::shared_mutex::unlock(); }
+    void unlock_shared() ASTD_RELEASE_SHARED() { std::shared_mutex::unlock_shared(); }
+};
+
+template<class Mutex>
+class [[nodiscard]] ASTD_SCOPED_CAPABILITY shared_lock : private std::shared_lock<Mutex> {
+public:
+    using mutex_type = Mutex;
+
+    // Constructors
+    shared_lock() noexcept : std::shared_lock<Mutex>() {}
+
+    explicit shared_lock( Mutex& m ) ASTD_ACQUIRE_SHARED(m)
+        : std::shared_lock<Mutex>(m) {}
+
+    shared_lock( Mutex& m, std::defer_lock_t ) noexcept ASTD_EXCLUDES(m)
+        : std::shared_lock<Mutex>(m, std::defer_lock) {}
+
+    shared_lock( Mutex& m, std::adopt_lock_t ) ASTD_REQUIRES_SHARED(m)
+        : std::shared_lock<Mutex>(m, std::adopt_lock) {}
+
+    shared_lock( Mutex& m, std::try_to_lock_t ) ASTD_MAYBE_ACQUIRE_SHARED(m)
+        : std::shared_lock<Mutex>(m, std::try_to_lock) {}
+
+    template< class Rep, class Period >
+    shared_lock( Mutex& m, const std::chrono::duration<Rep, Period>& timeout_duration ) ASTD_MAYBE_ACQUIRE_SHARED(m)
+        : std::shared_lock<Mutex>(m, timeout_duration) {}
+
+    template< class Clock, class Duration >
+    shared_lock( Mutex& m, const std::chrono::time_point<Clock, Duration>& timeout_time ) ASTD_MAYBE_ACQUIRE_SHARED(m)
+        : std::shared_lock<Mutex>(m, timeout_time) {}
+
+    // Copy
+    shared_lock( shared_lock& other ) = delete;
+    shared_lock& operator=( shared_lock& other ) = delete;
+
+    // Move
+    // TODO: Add annotations if Clang supports moving, swapping and disowning capabilities
+    shared_lock( shared_lock&& other ) noexcept ASTD_MOVE_CAPABILITIES(other)
+        : std::shared_lock<Mutex>(std::forward<shared_lock>(other)) {}
+
+    shared_lock& operator=( shared_lock&& other ) noexcept ASTD_MOVE_CAPABILITIES(other)
+    {
+        return std::shared_lock<Mutex>::operator=(std::forward<shared_lock>(other));
+    }
+
+    void swap( shared_lock& other ) noexcept ASTD_SWAP_CAPABILITIES(other) {
+        std::shared_lock<Mutex>::swap(other);
+    }
+
+    mutex_type* release() noexcept {
+        return std::shared_lock<Mutex>::release();
+    }
+
+    // Destructor
+    ~shared_lock() ASTD_RELEASE_IF_HELD() {}
+
+    void unlock() ASTD_RELEASE_SHARED() {
+        std::shared_lock<Mutex>::unlock();
+    }
+
+    void lock() ASTD_ACQUIRE_SHARED() {
+        std::shared_lock<Mutex>::lock();
+    }
+
+    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE_SHARED(true) {
+        return std::shared_lock<Mutex>::try_lock();
+    }
+
+    template< class Rep, class Period >
+    [[nodiscard]] bool try_lock_for( const std::chrono::duration<Rep, Period>& timeout_duration ) ASTD_TRY_ACQUIRE_SHARED(true) {
+        return std::shared_lock<Mutex>::try_lock_for(timeout_duration);
+    }
+
+    template< class Clock, class Duration >
+    [[nodiscard]] bool try_lock_until( const std::chrono::time_point<Clock, Duration>& timeout_time ) ASTD_TRY_ACQUIRE_SHARED(true) {
+        return std::shared_lock<Mutex>::try_lock_until(timeout_time);
+    }
+
+    // Clang thread safety analysis does not support alias analysis. Do not use this.
+    mutex_type* mutex() const noexcept {
+        return std::shared_lock<Mutex>::mutex();
+    }
+
+    // TODO: The following methods require a non-acquiring version of TRY_ACQUIRE
+    [[nodiscard]] bool owns_lock() const noexcept ASTD_CHECK_SHARED_CAPABILITY(true) {
+        return std::shared_lock<Mutex>::owns_lock();
+    }
+    [[nodiscard]] explicit operator bool() const noexcept ASTD_CHECK_SHARED_CAPABILITY(true) {
+        return std::shared_lock<Mutex>::operator bool();
+    }
+};
+
+} // namespace astd
+
+template< class Mutex >
+void swap( astd::shared_lock<Mutex>& lhs,
+           astd::shared_lock<Mutex>& rhs ) noexcept ASTD_SWAP_CAPABILITIES(lhs, rhs) {
+    lhs.swap(rhs);
+}
+
+#else
+
+namespace astd {
+
+using shared_mutex = std::shared_mutex;
+
+template<class Mutex>
+using shared_lock = std::shared_lock<Mutex>;
+
+} // namespace astd
+
+#endif
