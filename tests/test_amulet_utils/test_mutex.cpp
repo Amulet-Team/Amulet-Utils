5bd3e4313754657fd42b1ab84c5afc2183d428c3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-10 17:27:13 +0100

    Add shared mutex tests

--- tests/test_amulet_utils/test_mutex.cpp
+++ tests/test_amulet_utils/test_mutex.cpp
@@ -1,4 +1,5 @@
 #include <amulet/utils/threading/mutex.hpp>
+#include <amulet/utils/threading/shared_mutex.hpp>
 
 class TestMutex {
 private:
@@ -8,6 +9,7 @@ public:
     void test_access();
     void test_lock_unlock();
     void test_lock();
+    void test_unlock();
     void test_lock_lock();
     void test_try_lock();
     void test_try_lock_unlock_1();
@@ -30,6 +32,10 @@ void TestMutex::test_lock_unlock(){
 void TestMutex::test_lock(){
     m.lock(); // expected-note {{mutex acquired here}}
 } // expected-error {{mutex 'm' is still held at the end of function}}
+
+void TestMutex::test_unlock(){
+    m.unlock(); // expected-error {{releasing mutex 'm' that was not held}}
+}
 
 void TestMutex::test_lock_lock(){
     m.lock(); // expected-note 2 {{mutex acquired here}}
@@ -212,4 +218,197 @@ void TestUniqueLock::test_try_lock_constructor(){
 void TestUniqueLock::test_assign_lock_unlock(){
     astd::unique_lock<astd::mutex> lock1;
     astd::unique_lock lock2(m);
+}
+
+class TestSharedMutex {
+private:
+    astd::shared_mutex m;
+    int v ASTD_GUARDED_BY(m);
+public:
+    void test_access();
+    void test_lock_unlock();
+    void test_lock_shared_unlock_shared();
+    void test_lock_unlock_shared();
+    void test_lock_shared_unlock();
+    void test_lock();
+    void test_unlock();
+    void test_lock_shared();
+    void test_unlock_shared();
+    void test_lock_lock();
+    void test_lock_shared_lock_shared();
+    void test_try_lock();
+    void test_try_lock_unlock_1();
+    void test_try_lock_unlock_2();
+    void test_try_lock_unlock_3();
+    void test_try_lock_shared();
+    void test_try_lock_shared_unlock_shared_1();
+    void test_try_lock_shared_unlock_shared_2();
+    void test_try_lock_shared_unlock_shared_3();
+    void test_lock_try_lock();
+    void test_lock_shared_try_lock_shared();
+    void test();
+};
+
+void TestSharedMutex::test_access(){
+    v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+}
+
+void TestSharedMutex::test_lock_unlock(){
+    m.lock();
+    v += 1;
+    m.unlock();
+}
+
+void TestSharedMutex::test_lock_shared_unlock_shared(){
+    m.lock_shared();
+    v += 1;
+    m.unlock_shared();
+}
+
+void TestSharedMutex::test_lock_unlock_shared(){
+    m.lock();
+    v += 1;
+    m.unlock_shared(); // expected-error {{todo}}
+}
+
+void TestSharedMutex::test_lock_shared_unlock(){
+    m.lock_shared();
+    v += 1;
+    m.unlock(); // expected-error {{todo}}
+}
+
+void TestSharedMutex::test_lock(){
+    m.lock(); // expected-note {{shared mutex acquired here}}
+} // expected-error {{shared mutex 'm' is still held at the end of function}}
+
+void TestSharedMutex::test_unlock(){
+    m.unlock(); // expected-error {{releasing mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_lock_shared(){
+    m.lock_shared(); // expected-note {{shared mutex acquired here}}
+} // expected-error {{shared mutex 'm' is still held at the end of function}}
+
+void TestSharedMutex::test_unlock_shared(){
+    m.unlock_shared(); // expected-error {{releasing mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_lock_lock(){
+    m.lock(); // expected-note 2 {{shared mutex acquired here}}
+    m.lock(); // expected-error {{acquiring shared mutex 'm' that is already held}}
+} // expected-error {{shared mutex 'm' is still held at the end of function}}
+
+void TestSharedMutex::test_lock_shared_lock_shared(){
+    m.lock_shared(); // expected-note 2 {{shared mutex acquired here}}
+    m.lock_shared(); // expected-error {{acquiring shared mutex 'm' that is already held}}
+} // expected-error {{shared mutex 'm' is still held at the end of function}}
+
+void TestSharedMutex::test_try_lock(){
+    auto locked = m.try_lock(); // expected-note {{shared mutex acquired here}}
+    if (locked){
+        v += 1;
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+    }
+} // expected-error {{shared mutex 'm' is not held on every path through here}}
+
+void TestSharedMutex::test_try_lock_unlock_1(){
+    auto locked = m.try_lock();
+    if (locked){
+        v += 1;
+        m.unlock();
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+        m.unlock(); // expected-error {{releasing shared mutex 'm' that was not held}}
+    }
+}
+
+void TestSharedMutex::test_try_lock_unlock_2(){
+    auto locked = m.try_lock(); // expected-note {{shared mutex acquired here}}
+    if (locked){
+        v += 1;
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+    }
+    m.unlock(); // expected-error {{shared mutex 'm' is not held on every path through here}} expected-error {{releasing mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_try_lock_unlock_3(){
+    auto locked = m.try_lock();
+    m.unlock(); // expected-error {{releasing shared mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_try_lock_shared(){
+    auto locked = m.try_lock_shared(); // expected-note {{shared mutex acquired here}}
+    if (locked){
+        v += 1;
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+    }
+} // expected-error {{shared mutex 'm' is not held on every path through here}}
+
+void TestSharedMutex::test_try_lock_shared_unlock_shared_1(){
+    auto locked = m.try_lock_shared();
+    if (locked){
+        v += 1;
+        m.unlock_shared();
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+        m.unlock(); // expected-error {{releasing shared mutex 'm' that was not held}}
+    }
+}
+
+void TestSharedMutex::test_try_lock_shared_unlock_shared_2(){
+    auto locked = m.try_lock_shared(); // expected-note {{shared mutex acquired here}}
+    if (locked){
+        v += 1;
+    } else {
+        v += 1; // expected-error {{writing variable 'v' requires holding shared mutex 'm' exclusively}}
+    }
+    m.unlock_shared(); // expected-error {{shared mutex 'm' is not held on every path through here}} expected-error {{releasing mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_try_lock_shared_unlock_shared_3(){
+    auto locked = m.try_lock_shared();
+    m.unlock(); // expected-error {{releasing shared mutex 'm' that was not held}}
+}
+
+void TestSharedMutex::test_lock_try_lock(){
+    m.lock(); // expected-note 2 {{shared mutex acquired here}}
+    auto locked = m.try_lock(); // expected-error {{acquiring shared mutex 'm' that is already held}}
+    if (locked){
+        v += 1;
+        m.unlock();
+    }
+} // expected-error {{shared mutex 'm' is not held on every path through here}}
+
+void TestSharedMutex::test_lock_shared_try_lock_shared(){
+    m.lock_shared(); // expected-note 2 {{shared mutex acquired here}}
+    auto locked = m.try_lock_shared(); // expected-error {{acquiring shared mutex 'm' that is already held}}
+    if (locked){
+        v += 1;
+        m.unlock_shared();
+    }
+} // expected-error {{shared mutex 'm' is not held on every path through here}}
+
+void TestSharedMutex::test() {
+    m.lock();
+    v += 1;
+    m.unlock();
+
+    m.lock_shared();
+    v += 1;
+    m.unlock_shared();
+
+    auto locked = m.try_lock();
+    if (locked){
+        v += 1;
+        m.unlock();
+    }
+
+    auto locked = m.try_lock_shared();
+    if (locked){
+        v += 1;
+        m.unlock_shared();
+    }
 }
