30925f4bf1165bba2f1c10a33c506f9ea551e71a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-12 14:50:50 +0100

    Revert "Add shared release if held macro"
    This reverts commit ec9d52b1c6a92737f4d1870cb189d3d4e1320ce2.

--- src/amulet/utils/threading/shared_mutex.hpp
+++ src/amulet/utils/threading/shared_mutex.hpp
@@ -73,7 +73,7 @@ public:
     }
 
     // Destructor
-    ~shared_lock() ASTD_RELEASE_IF_HELD_SHARED() {}
+    ~shared_lock() ASTD_RELEASE_IF_HELD() {}
 
     void unlock() ASTD_RELEASE_SHARED() {
         std::shared_lock<Mutex>::unlock();
