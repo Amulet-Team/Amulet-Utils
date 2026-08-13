ab09e4e4a75eae699d40af659c7555b3ef19b352
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-13 11:24:25 +0100

    Split shared capability into base class

--- src/amulet/utils/threading/shared_mutex.hpp
+++ src/amulet/utils/threading/shared_mutex.hpp
@@ -5,19 +5,23 @@
 #if defined(AMULET_THREAD_SAFETY_ANALYSIS)
 
 #include <chrono>
+#include <type_traits>
 
 #include "thread_safety.hpp"
 
 namespace astd {
 
-class ASTD_CAPABILITY("mutex") shared_mutex : private std::shared_mutex {
+class base_shared_mutex {
 public:
     // Clang's thread safety analysis does not track only shared mode.
     // This is needed to stop a uniquely locked mutex being adopted by a shared_lock.
     // This capability will be acquired only when locked in shared mode.
     class ASTD_CAPABILITY("shared-mode") SharedMode {};
     SharedMode shared;
+};
 
+class ASTD_CAPABILITY("mutex") shared_mutex : private std::shared_mutex, public base_shared_mutex {
+public:
     using std::shared_mutex::shared_mutex;
     void lock() ASTD_ACQUIRE_UNIQUE() { std::shared_mutex::lock(); }
     void lock_shared() ASTD_ACQUIRE_SHARED() ASTD_ACQUIRE_SHARED(shared) { std::shared_mutex::lock_shared(); }
@@ -28,6 +32,7 @@ public:
 };
 
 template<class Mutex>
+    requires std::is_base_of_v<base_shared_mutex, Mutex>
 class [[nodiscard]] ASTD_SCOPED_CAPABILITY shared_lock : private std::shared_lock<Mutex> {
 public:
     using mutex_type = Mutex;
