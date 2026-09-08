f33b8087b48e57ad9c7fd45327c835be1980b3f5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-09-08 14:49:09 +0100

    Move and refactor try acquire and check macros

--- src/amulet/utils/threading/thread_safety.hpp
+++ src/amulet/utils/threading/thread_safety.hpp
@@ -40,6 +40,12 @@
 
 #define ASTD_ACQUIRE_SHARED(...) \
     __ASTD_ATTRIBUTE__(acquire_shared_capability(__VA_ARGS__))
+#define ASTD_TRY_ACQUIRE_UNIQUE(value, ...) \
+    __ASTD_ATTRIBUTE__(try_acquire_capability(value, __VA_ARGS__))
+
+#define ASTD_TRY_ACQUIRE_SHARED(value, ...) \
+    __ASTD_ATTRIBUTE__(try_acquire_shared_capability(value, __VA_ARGS__))
+
 
 #define ASTD_RELEASE(...) \
     __ASTD_ATTRIBUTE__(release_capability(__VA_ARGS__))
@@ -52,11 +58,6 @@
 #define ASTD_RELEASE_GENERIC(...) \
     __ASTD_ATTRIBUTE__(release_generic_capability(__VA_ARGS__))
 
-#define ASTD_TRY_ACQUIRE_UNIQUE(...) \
-    __ASTD_ATTRIBUTE__(try_acquire_capability(__VA_ARGS__))
-
-#define ASTD_TRY_ACQUIRE_SHARED(...) \
-    __ASTD_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
 
 #define ASTD_EXCLUDES(...) \
     __ASTD_ATTRIBUTE__(locks_excluded(__VA_ARGS__))
@@ -85,8 +86,8 @@
 // Required by owns_lock to inspect the lock state without acquiring it.
 // Works like try_acquire_[shared_]capability but can be used in any context.
 // Currently they are aliased to try_acquire_[shared_]capability so can only be used in the unlocked state.
-#define ASTD_CHECK_UNIQUE_CAPABILITY(...) ASTD_TRY_ACQUIRE_UNIQUE(__VA_ARGS__)
-#define ASTD_CHECK_SHARED_CAPABILITY(...) ASTD_TRY_ACQUIRE_SHARED(__VA_ARGS__)
+#define ASTD_CHECK_UNIQUE_CAPABILITY(value, ...) ASTD_TRY_ACQUIRE_UNIQUE(value, __VA_ARGS__)
+#define ASTD_CHECK_SHARED_CAPABILITY(value, ...) ASTD_TRY_ACQUIRE_SHARED(value, __VA_ARGS__)
 
 // Required by lock destructors when the capability is not held.
 // Release the capability, regardless of initial value.
