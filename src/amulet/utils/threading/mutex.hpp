31ec92363f2b33d786d45b0720b92cc1846c36d2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-09-10 09:30:22 +0100

    Fix attribute access

--- src/amulet/utils/threading/mutex.hpp
+++ src/amulet/utils/threading/mutex.hpp
@@ -13,17 +13,17 @@ namespace astd {
 class ASTD_CAPABILITY("mutex") mutex : private std::mutex, public base_mutex {
 public:
     using std::mutex::mutex;
-    void lock() ASTD_ACQUIRE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::mutex::lock(); }
-    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE_UNIQUE(true, this) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::mutex::try_lock(); }
-    void unlock() ASTD_RELEASE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::mutex::unlock(); }
+    void lock() ASTD_ACQUIRE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::mutex::lock(); }
+    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE_UNIQUE(true, (*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::mutex::try_lock(); }
+    void unlock() ASTD_RELEASE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::mutex::unlock(); }
 };
 
 class ASTD_CAPABILITY("recursive mutex") recursive_mutex : private std::recursive_mutex, public base_mutex {
 public:
     using std::recursive_mutex::recursive_mutex;
-    void lock() ASTD_ACQUIRE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::recursive_mutex::lock(); }
-    [[nodiscard]] bool try_lock() noexcept ASTD_TRY_ACQUIRE_UNIQUE(true, this) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::recursive_mutex::try_lock(); }
-    void unlock() ASTD_RELEASE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::recursive_mutex::unlock(); }
+    void lock() ASTD_ACQUIRE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::recursive_mutex::lock(); }
+    [[nodiscard]] bool try_lock() noexcept ASTD_TRY_ACQUIRE_UNIQUE(true, (*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::recursive_mutex::try_lock(); }
+    void unlock() ASTD_RELEASE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::recursive_mutex::unlock(); }
 };
 
 template<class Mutex>
--- src/amulet/utils/threading/shared_mutex.hpp
+++ src/amulet/utils/threading/shared_mutex.hpp
@@ -14,12 +14,12 @@ namespace astd {
 class ASTD_CAPABILITY("mutex") shared_mutex : private std::shared_mutex, public base_shared_mutex {
 public:
     using std::shared_mutex::shared_mutex;
-    void lock() ASTD_ACQUIRE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::lock(); }
-    void lock_shared() ASTD_ACQUIRE_SHARED(this) ASTD_ACQUIRE_SHARED(shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::lock_shared(); }
-    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE_UNIQUE(true, this) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::shared_mutex::try_lock(); }
-    [[nodiscard]] bool try_lock_shared() ASTD_TRY_ACQUIRE_SHARED(true, this) ASTD_TRY_ACQUIRE_SHARED(true, shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::shared_mutex::try_lock_shared(); }
-    void unlock() ASTD_RELEASE_UNIQUE(this) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::unlock(); }
-    void unlock_shared() ASTD_RELEASE_SHARED(this) ASTD_RELEASE_SHARED(shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::unlock_shared(); }
+    void lock() ASTD_ACQUIRE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::lock(); }
+    void lock_shared() ASTD_ACQUIRE_SHARED((*this)) ASTD_ACQUIRE_SHARED(shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::lock_shared(); }
+    [[nodiscard]] bool try_lock() ASTD_TRY_ACQUIRE_UNIQUE(true, (*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::shared_mutex::try_lock(); }
+    [[nodiscard]] bool try_lock_shared() ASTD_TRY_ACQUIRE_SHARED(true, (*this)) ASTD_TRY_ACQUIRE_SHARED(true, shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { return std::shared_mutex::try_lock_shared(); }
+    void unlock() ASTD_RELEASE_UNIQUE((*this)) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::unlock(); }
+    void unlock_shared() ASTD_RELEASE_SHARED((*this)) ASTD_RELEASE_SHARED(shared_capability) ASTD_NO_THREAD_SAFETY_ANALYSIS { std::shared_mutex::unlock_shared(); }
 };
 
 template<class Mutex>
